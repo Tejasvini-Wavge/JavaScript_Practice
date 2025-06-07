97febe18cd93b1c7677c3eb8fc41cdacce7e0e9a
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-06-07 20:09:43 +0530

    loops.

---
+++ loops.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>loop</title>
+</head>
+<body>
+    <script>
+        //for loop
+        for(var i=1;i<=5;i++){
+            document.write(i + "<br>")
+        }
+
+    document.write("<br>");
+    document.write("<br>");
+      //while loop
+      var i=1;
+      while(i<=5){
+        document.write(i+"<br>");
+        i++;
+      }
+
+    document.write("<br>");
+    document.write("<br>");
+    //do-while-loop
+    
+    var i=1;
+    do{
+        document.write(i+"<br>");
+        i++;
+    }while(i<=5){
+        document.write(i+"<br>");
+        i++;
+      }
+
+    </script>
+    
+</body>
+</html>
