128699e12db5c52c6b2b99aeaac8b47b703228cd
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-06-08 20:47:03 +0530

    Create Date.

---
+++ Date.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        let currentDate=new Date();
+        console.log(currentDate); //output the current date and time
+
+        let specificDate=new Date("2025-03-11");
+        console.log(specificDate); 
+        
+        let specificDatetime=new Date();
+        console.log(specificDatetime);
+
+        
+    </script>
+</body>
+</html>
