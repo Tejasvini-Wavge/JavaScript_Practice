d4411d5b2b22f3449e55443a37b63546430336a5
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-06-08 20:44:38 +0530

    Create boolean.

---
+++ boolean.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        let isTrue=new Boolean(true);
+        let isFalse=new Boolean(false);
+        console.log(isTrue);
+        console.log(isFalse);
+       </script>
+</body>
+    
+    
+</html>
