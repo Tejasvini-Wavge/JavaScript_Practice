93efc0c6fe9d0e570ec58d922b8667cb5e424a17
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-06-11 20:20:38 +0530

    Create Function. in js

---
+++ Function.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        function demo() //function declaration
+        {
+            document.writeln("hello"); //fun defination
+            document.writeln("<br>");
+            document.writeln("i am tejasvini");
+             document.writeln("<br>");
+            document.writeln("i am frontend developer");
+        }
+        demo(); //function calling
+    </script>
+    
+</body>
+</html>
