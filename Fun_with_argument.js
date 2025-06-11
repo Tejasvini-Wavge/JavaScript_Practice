8d6ccf2f0c1df6f225bd9d34878cc88376a38520
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-06-11 20:25:04 +0530

    Create Fun_with_argument.

---
+++ Fun_with_argument.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        //fun with argument
+        function name(fname,lname) //function declaration
+        {
+            document.writeln("hello...." + fname +" "+lname); //fun defination
+            
+        }
+        name("teju","wavge"); //function calling
+    </script>
+    
+</body>
+</html>
